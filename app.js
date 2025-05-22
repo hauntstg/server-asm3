30ad3eaee25c9020ebb8e9fa1e08ee93725da456
Hau Nguyen <nthau.it96@gmail.com>
2025-05-22 21:46:58 +0700

    add real client site to app js

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ const messageAdminRoutes = require("./routes/admin/messageAdmin");
 
 const allowedOrigins = [
   "http://localhost:3000", // client
+  "https://client-asm3-gli2gylv6-hau-nguyens-projects-2ebe7b1e.vercel.app/",
   "http://localhost:3001", // admin
 ];
 
