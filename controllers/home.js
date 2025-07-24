6cdfd64d07f80e9b2ac1a1bdb33bd4b15f7d9175
Hau Nguyen <nthau.it96@gmail.com>
2025-07-24 22:47:35 +0700

    phan trang

--- controllers/home.js
+++ controllers/home.js
@@ -1,11 +1,11 @@
 const Product = require("../models/product");
 
-exports.getProducts = async (req, res, next) => {
+exports.getProductList = async (req, res, next) => {
   try {
-    const { category, name } = req.query;
+    const { category, name, page } = req.query;
 
     const filter = {};
-    console.log(category);
+    console.log(category + " " + name + " " + page);
     if (category) {
       filter.category = category;
     }
@@ -16,6 +16,56 @@ exports.getProducts = async (req, res, next) => {
 
     const products = await Product.find(filter);
     res.status(200).json(products);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.getProducts = async (req, res, next) => {
+  try {
+    const currentPage = req.query.page || 1;
+    const name = req.query.name || "";
+    const category = req.query.category;
+
+    const filter = {};
+
+    if (category) {
+      filter.category = category;
+    }
+
+    if (name) {
+      filter.name = { $regex: name, $options: "i" };
+    }
+
+    const perPage = 9;
+    const totalItems = await Product.countDocuments(filter);
+    const totalPages = Math.ceil(totalItems / perPage);
+    const fromIndex = (currentPage - 1) * perPage + 1;
+    const toIndex = Math.min(currentPage * perPage, totalItems);
+
+    const products = await Product.find(filter)
+      .skip((currentPage - 1) * perPage)
+      .limit(perPage);
+    // console.log(products.length);
+    console.log(
+      "c: " + category,
+      "n: " + name,
+      totalPages,
+      fromIndex,
+      toIndex,
+      totalItems
+    );
+    res.status(200).json({
+      products,
+      pagination: {
+        totalItems,
+        perPage,
+        totalPages,
+        fromIndex,
+        toIndex,
+        currentPage,
+      },
+    });
   } catch (error) {
     console.log(error);
   }
--- routes/home.js
+++ routes/home.js
@@ -3,6 +3,10 @@ const homeController = require("../controllers/home");
 
 const router = express.Router();
 
+// danh sách sản phẩm không phân trang
+router.get("/product-list", homeController.getProductList);
+
+// danh sách sản phẩm có phân trang
 router.get("/products", homeController.getProducts);
 
 router.get("/products/:productId", homeController.getProduct);
